b5f98c535c22b56e9f1226efb67a0043cede1746
Stan Girard <girard.stanislas@gmail.com>
2023-05-07 01:11:00 +0200

    feat(doc): updated

--- README.md
+++ README.md
@@ -1,4 +1,64 @@
-# whisper
+# Speech Digest
 
+This Streamlit app allows users to upload an audio file, transcribe the audio using OpenAI's Whisper ASR, and then summarize the transcription using OpenAI's GPT-3.5-turbo language model.
 
-Streamlit run
+## Features
+
+- Upload audio files in supported formats (m4a, mp3, webm, mp4, mpga, wav, and mpeg)
+- Transcribe audio using OpenAI's Whisper ASR
+- Summarize transcribed audio using OpenAI's GPT-3.5-turbo
+- Provide custom API key and select the desired model
+
+## Installation
+
+### Prerequisites
+
+- Python 3.6 or higher
+- Streamlit
+- OpenAI Python library (v0.27.0 or higher)
+
+### Steps
+
+1. Clone the repository:
+
+```
+git clone https://github.com/StanGirard/speechdigest
+```
+
+2. Change into the repository's directory:
+
+```
+cd speechdigest
+```
+
+3. Install the required packages:
+
+```
+pip install -r requirements.txt
+```
+
+4. Set your OpenAI API key as an environment variable:
+
+```
+export OPENAI_API_KEY="your-api-key"
+```
+
+5. Run the Streamlit app:
+
+```
+streamlit run app.py
+```
+
+The app will now be accessible at `http://localhost:8501` in your browser.
+
+## Usage
+
+1. Open the app in your browser.
+2. Enter your OpenAI API key and select the desired model (GPT-4 or GPT-3.5-turbo) using the input fields.
+3. Upload an audio file in a supported format using the file uploader.
+4. The app will transcribe the audio using Whisper ASR and display the transcription.
+5. The app will then summarize the transcription using the selected language model and display the summary.
+
+## Contributing
+
+Feel free to submit issues, feature requests, or pull requests. We appreciate any contribution to improve the app.
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 def transcribe_audio(api_key, audio_file):
     openai.api_key = api_key
     with BytesIO(audio_file.read()) as audio_bytes:
-        # Get the extension of the uloaded file
+        # Get the extension of the uploaded file
         file_extension = os.path.splitext(audio_file.name)[-1]
         
         # Create a temporary file with the uploaded audio data and the correct extension
@@ -38,6 +38,7 @@ def summarize_transcript(api_key, transcript, model):
     return summary
 
 # Streamlit app
+st.set_page_config(page_title="Speech Digest", page_icon="🎙️")
 st.title("Audio Transcription and Summarization")
 st.write("Upload an audio file, transcribe it using Whisper, and summarize the transcription using your selected model.")
 
@@ -47,14 +48,42 @@ model = st.selectbox("Select a model:", models)
 
 uploaded_audio = st.file_uploader("Upload an audio file", type=['m4a', 'mp3', 'webm', 'mp4', 'mpga', 'wav', 'mpeg'], accept_multiple_files=False)
 
+
 if uploaded_audio:
     if api_key:
-        st.write("Transcribing the audio...")
+        st.markdown("Transcribing the audio...")
         transcript = transcribe_audio(api_key, uploaded_audio)
-        st.write("Transcription:", transcript)
+        st.markdown(f"###  Transcription:\n\n<details><summary>Click to view</summary><p><pre><code>{transcript}</code></pre></p></details>", unsafe_allow_html=True)
 
-        st.write("Summarizing the transcription...")
+        st.markdown("Summarizing the transcription...")
         summary = summarize_transcript(api_key, transcript, model)
-        st.write("Summary:", summary)
+        st.markdown(f"### Summary:")
+        st.write(summary)
     else:
         st.error("Please enter a valid OpenAI API key.")
+
+
+st.markdown(
+    """
+    ---
+    ### Source code and contact information
+    - The source code for this app can be found on GitHub: [SpeechDigest](https://github.com/StanGirard/speechdigest)
+    - If you have any questions or comments, feel free to reach out to me on Twitter: [@_StanGirard](https://twitter.com/_StanGirard)
+    """
+)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown(
+        """
+        [![Tweet](https://img.shields.io/twitter/url?url=https%3A%2F%2Fgithub.com%2FStanGirard%2Fspeechdigest)](https://twitter.com/intent/tweet?url=https://github.com/StanGirard/speechdigest&text=Check%20out%20this%20awesome%20Speech%20Digest%20app%20built%20with%20Streamlit!%20%23speechdigest%20%23streamlit)
+        """
+    )
+
+with col2:
+    st.markdown(
+        """
+        [![GitHub Stars](https://img.shields.io/github/stars/StanGirard/speechdigest?style=social)](https://github.com/StanGirard/speechdigest/stargazers)
+        """
+    )
